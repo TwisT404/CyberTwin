2f1a33ff47dab50b2d6ccfbabf13a11f3b8b80a4
TwisT404 <cedric.tanghe1303@gmail.com>
2026-06-12 15:05:33 +0200

    Add router and moved into views folder

---
+++ Front/src/components/HeaderNav.vue
@@ -0,0 +1,13 @@
+<template>
+    <header>
+        <div>
+            <a href="/dashboard">Dashboard</a>
+            <a href="/entreprises">Entreprise</a>
+            <a href="/actifs">Actifs</a>
+            <a href="/vulnerabilites">Vulnérabilités</a>
+        </div>
+        <div>
+            <p>CyberTwin</p>
+        </div>
+    </header>
+</template>
--- Front/src/router/index.js
+++ Front/src/router/index.js
@@ -1,8 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const Dashboard = () => import('../views/Dashboard.vue')
+const Home = () => import('../App.vue')
+const Entreprises = () => import('../views/Entreprise/Entreprise.vue')
+const Actifs = () => import('../views/Actif/Actif.vue')
+const Vulnerabilite = () => import('../views/Vulnerabilite/Vulnerabilite.vue')
+
+
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/entreprises', component: Entreprises },
+    {
+        path: '/dashboard',
+        component: Dashboard,
+    },
+    {
+        path: '/actifs',
+        component: Actifs,
+    },
+        {
+        path: '/vulnerabilites',
+        component: Vulnerabilite,
+    }
+]
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+    history: createWebHistory(),
+    routes
 })
 
 export default router
