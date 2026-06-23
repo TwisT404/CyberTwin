f5ec006c6db1122ce0d6d2bc92c988eb2b769e46
TwisT404 <cedric.tanghe1303@gmail.com>
2026-06-23 18:36:34 +0200

    Modify Actif Page and new Vulenrability page

--- Front/src/views/Actif/Actif.vue
+++ Front/src/views/Actif/Actif.vue
@@ -1,1 +1,18 @@
-<template></template>
+<template>
+    <h1>Gestion des actifs :</h1>
+    <a href="" id="addActif">Ajouter un actif</a>
+    <section>
+        <div class="block">
+            <h2>Type d'actifs : Serveur Web</h2>
+            <div class="link">
+                <a>Modifier</a>
+                <a>Supprimer</a>
+            </div>      
+        </div>
+    </section>
+</template>
+
+
+<style scoped>
+@import url('./../../assets/css/Actifs/Actif.css');
+</style>
--- Front/src/views/Vulnerabilite/Vulnerabilite.vue
+++ Front/src/views/Vulnerabilite/Vulnerabilite.vue
@@ -1,1 +1,20 @@
-<template></template>
+<template>
+    <h1>Les différents vulnérabilités :</h1>
+    <a href="" id="addVul">Associer une vulnérabilité</a>
+    <section>
+        <div class="block">
+            <h2>Type d'actifs : Serveur Web</h2>
+            <p>Vulnérabilité : Logiciel obsolète</p>
+            <p>Niveau de criticité : Faible</p>
+            <p>Entreprise concernée : Entreprise</p>
+            <div class="link">
+                <a>Modifier</a>
+                <a>Supprimer</a>
+            </div>      
+        </div>
+    </section>
+</template>
+
+<style scoped>
+@import url('./../../assets/css/Vulnerabilite/Vulnerabilite.css');
+</style>
